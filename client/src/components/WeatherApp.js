0d4ea040a05b0b784caa9553237529f16bd69a86
Dhairya Khara <kharadhairya@gmail.com>
2021-09-08 17:33:34 -0400

    main page component

---
+++ client/src/components/WeatherApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Searchbox from './Searchbox'
+
+export default class WeatherApp extends React.Component{
+    render(){
+        return(
+            <div>
+                <Searchbox/>
+            </div>
+        )
+    }
+}
