0f2b850173a89ae36ef45d9b32a63bcf2efa8734
Dhairya Khara <kharadhairya@gmail.com>
2021-09-08 17:36:55 -0400

    made primary api route for weather

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const forecast = require('./utils/forecast')
+
+const geocode = require('./utils/geocode')
 
 const app = express()
 const port = process.env.PORT || 8080
 
-app.get('/', (req,res)=>{
-    res.send("yo")
+app.get('/weather', async (req,res)=>{
+    console.log(req.query.address)
+    if(!req.query.address){
+        res.send("Error. Please provide location")
+    }
+    else{
+        const geocodeData = await geocode(req.query.address)
+        const forecastData = await forecast(geocodeData.latitude, geocodeData.longitude)
+        console.log(forecastData)
+        res.send("yo")
+        
+    }
+    
 })
 
 app.listen(port, ()=>{
