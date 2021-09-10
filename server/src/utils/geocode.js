117ba8c611c7787a22684d4d80995367b27b7fc6
Dhairya Khara <kharadhairya@gmail.com>
2021-09-10 17:03:51 -0400

    reject promise when cant find location

--- server/src/utils/geocode.js
+++ server/src/utils/geocode.js
@@ -4,7 +4,7 @@ const geocode = async (address) =>{
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token='+encodeURIComponent(process.env.mapBoxKey)
     return new Promise((resolve, reject)=>{
         request({url, json:true}, (error, response)=>{
-            if(error){
+            if(!response.body.features[0]){
                 reject("Can't find location")
                 return
             }
