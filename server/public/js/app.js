0b9a1c66143aa0b7d2b04013a10855beeeb192f2
Dhairya Khara <kharadhairya@gmail.com>
2021-09-10 17:03:27 -0400

    fixed rendering when cant find location

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -51,8 +51,8 @@ const precipProbability7 = document.querySelector('#precipProbability7')
 
 
 //function to determine icon
-const determineIcon =  (imageForTemp, imageCode) =>{
-    imageForTemp.innerHTML = "<img src=http://openweathermap.org/img/wn/"+imageCode+"@2x.png width=100 height=100></img>"
+const determineIcon = (imageForTemp, imageCode) => {
+    imageForTemp.innerHTML = "<img src=http://openweathermap.org/img/wn/" + imageCode + "@2x.png width=100 height=100></img>"
 }
 
 weatherForm.addEventListener('submit', (event) => {
@@ -67,11 +67,51 @@ weatherForm.addEventListener('submit', (event) => {
         response.json().then((data) => {
 
             if (data.error) {
-                locationWeb.textContent = data.error
+                //currently
+                locationWeb.textContent = data.errorText
                 summaryWeb.textContent = ''
                 temperatureWeb.textContent = ''
                 precipProbabilityWeb.textContent = ''
-                maxMinFeel.content = ''
+                maxMinFeel.textContent = ''
+                imageForTemp.innerHTML = ''
+
+                //daily
+                day2.textContent = ''
+                imageForTemp2.innerHTML = ''
+                temperatureWeb2.textContent = ''
+                maxMinFeel2.textContent = ''
+                precipProbability2.textContent = ''
+
+                day3.textContent = ''
+                imageForTemp3.innerHTML = ''
+                temperatureWeb3.textContent = ''
+                maxMinFeel3.textContent = ''
+                precipProbability3.textContent = ''
+
+                day4.textContent = ''
+                imageForTemp4.innerHTML = ''
+                temperatureWeb4.textContent = ''
+                maxMinFeel4.textContent = ''
+                precipProbability4.textContent = ''
+
+                day5.textContent = ''
+                imageForTemp5.innerHTML = ''
+                temperatureWeb5.textContent = ''
+                maxMinFeel5.textContent = ''
+                precipProbability5.textContent = ''
+
+                day6.textContent = ''
+                imageForTemp6.innerHTML = ''
+                temperatureWeb6.textContent = ''
+                maxMinFeel6.textContent = ''
+                precipProbability6.textContent = ''
+
+                day7.textContent = ''
+                imageForTemp7.innerHTML = ''
+                temperatureWeb7.textContent = ''
+                maxMinFeel7.textContent = ''
+                precipProbability7.textContent = ''
+
             }
             else {
                 //Currently Start
@@ -106,31 +146,31 @@ weatherForm.addEventListener('submit', (event) => {
                 maxMinFeel2.textContent = 'Feels Like: ' + Math.round(data.day2FeelsLikeMax) + '°/' + Math.round(data.day2FeelsLikeMin) + '°'
                 precipProbability2.textContent = 'Precipitation: ' + Math.round(data.day2PrecipProbability * 100) + '%'
 
-                day3.textContent = data.day3Day  + ": " + data.day3Description
+                day3.textContent = data.day3Day + ": " + data.day3Description
                 determineIcon(imageForTemp3, data.day3Icon)
                 temperatureWeb3.textContent = Math.round(data.day3Max) + "°/" + Math.round(data.day3Min) + '°'
                 maxMinFeel3.textContent = 'Feels Like: ' + Math.round(data.day3FeelsLikeMax) + '°/' + Math.round(data.day3FeelsLikeMin) + '°'
                 precipProbability3.textContent = 'Precipitation: ' + Math.round(data.day3PrecipProbability * 100) + '%'
 
-                day4.textContent = data.day4Day  + ": " + data.day4Description
+                day4.textContent = data.day4Day + ": " + data.day4Description
                 determineIcon(imageForTemp4, data.day4Icon)
                 temperatureWeb4.textContent = Math.round(data.day4Max) + "°/" + Math.round(data.day4Min) + '°'
                 maxMinFeel4.textContent = 'Feels Like: ' + Math.round(data.day4FeelsLikeMax) + '°/' + Math.round(data.day4FeelsLikeMin) + '°'
                 precipProbability4.textContent = 'Precipitation: ' + Math.round(data.day4PrecipProbability * 100) + '%'
 
-                day5.textContent = data.day5Day  + ": " + data.day5Description
+                day5.textContent = data.day5Day + ": " + data.day5Description
                 determineIcon(imageForTemp5, data.day5Icon)
                 temperatureWeb5.textContent = Math.round(data.day5Max) + "°/" + Math.round(data.day5Min) + '°'
                 maxMinFeel5.textContent = 'Feels Like: ' + Math.round(data.day5FeelsLikeMax) + '°/' + Math.round(data.day5FeelsLikeMin) + '°'
                 precipProbability5.textContent = 'Precipitation: ' + Math.round(data.day5PrecipProbability * 100) + '%'
 
-                day6.textContent = data.day6Day  + ": " + data.day6Description
+                day6.textContent = data.day6Day + ": " + data.day6Description
                 determineIcon(imageForTemp6, data.day6Icon)
                 temperatureWeb6.textContent = Math.round(data.day6Max) + "°/" + Math.round(data.day6Min) + '°'
                 maxMinFeel6.textContent = 'Feels Like: ' + Math.round(data.day6FeelsLikeMax) + '°/' + Math.round(data.day6FeelsLikeMin) + '°'
                 precipProbability6.textContent = 'Precipitation: ' + Math.round(data.day6PrecipProbability * 100) + '%'
 
-                day7.textContent = data.day7Day  + ": " + data.day7Description
+                day7.textContent = data.day7Day + ": " + data.day7Description
                 determineIcon(imageForTemp7, data.day7Icon)
                 temperatureWeb7.textContent = Math.round(data.day7Max) + "°/" + Math.round(data.day7Min) + '°'
                 maxMinFeel7.textContent = 'Feels Like: ' + Math.round(data.day7FeelsLikeMax) + '°/' + Math.round(data.day7FeelsLikeMin) + '°'
@@ -138,6 +178,9 @@ weatherForm.addEventListener('submit', (event) => {
             }
 
         })
+    }).catch(() => {
+        locationWeb.textContent = "Can't Find Location!"
+
     })
 
 })
