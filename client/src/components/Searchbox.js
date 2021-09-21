fe8dfc4e56dffbbb7a3878f204755f53cd9c2b5b
Dhairya Khara <kharadhairya@gmail.com>
2021-09-21 18:12:14 -0400

    styled main searchbox

--- client/src/components/Searchbox.js
+++ client/src/components/Searchbox.js
@@ -1,20 +1,39 @@
 import React from 'react'
 
-export default class Searchbox extends React.Component{
-    
-    searchForLocation = (event)=>{
+import LocationSummary  from './LocationSummary'
+
+export default class Searchbox extends React.Component {
+
+    searchForLocation = (event) => {
         event.preventDefault()
         const inputValue = event.target.elements.location.value
-        console.log(inputValue)
+        //make call to api
+        fetch('http://localhost:8080/weather?address='+ encodeURIComponent(inputValue)).then((response)=>{
+            // console.log(JSON.parse(response))
+            return response.json()
+        }).then((response)=>{
+            console.log(response)
+            return(
+                <LocationSummary/>
+            )
+        })
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-               <form className = "locationForm" onSubmit = {this.searchForLocation}>
+                {/* <form className = "locationForm" onSubmit = {this.searchForLocation}>
                    <input className = "textBox" type = "text" name = "location"></input>
                    <button className = "button">Search</button>
-               </form>
+               </form> */}
+                <div className="wrapper">
+                    <div className="searchBar">
+                        <form onSubmit = {this.searchForLocation}>
+                            <input id="searchQueryInput" type="text" name="location" placeholder="Search" />
+                            <button id="searchQuerySubmit" type="submit" name="searchQuerySubmit" />
+                        </form>
+                    </div>
+                </div>
             </div>
         )
     }
