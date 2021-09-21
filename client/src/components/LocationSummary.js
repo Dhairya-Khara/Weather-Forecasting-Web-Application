d20f9e967eed0824444dbfb407f28f82ff8f385b
Dhairya Khara <kharadhairya@gmail.com>
2021-09-21 18:13:52 -0400

    component to render location summary

---
+++ client/src/components/LocationSummary.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class LocationSummary extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>This is my location summary</p>
+            </div>
+        )
+    }
+}
